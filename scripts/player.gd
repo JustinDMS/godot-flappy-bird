2af6eb568554cb481f0de992d34351766193cb6d
JustinDMS <90155396+JustinDMS@users.noreply.github.com>
2024-08-21 16:05:26 -0500

    Added rotation based on velocity

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,12 @@ extends CharacterBody2D
 const GRAVITY := 1500.0
 const JUMP_SPEED := 500.0
 
+const MAX_TILT_FORWARD : float = 75.0 ## Degrees
+const MAX_TILT_BACKWARD : float = -10.0 ## Degrees
+const TILT_WEIGHT : float = 10.0
+
+@onready var sprite : Sprite2D = $Sprite2D
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action("jump"):
 		if event.is_pressed() and not event.is_echo():
@@ -11,6 +17,9 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	velocity.y += GRAVITY * delta
 	move_and_slide()
+	
+	# Rotate sprite based on velocity
+	sprite.rotation_degrees = remap(velocity.y, -500, 1000, MAX_TILT_BACKWARD, MAX_TILT_FORWARD)
 	
 	if get_slide_collision_count() >= 1:
 		Game.game_over()
