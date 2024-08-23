9a3aab924c9097ad69765263d5d368a7282aa400
JustinDMS <90155396+JustinDMS@users.noreply.github.com>
2024-08-23 12:53:48 -0500

    Added score display juice

--- scripts/main.gd
+++ scripts/main.gd
@@ -18,9 +18,20 @@ func _input(event: InputEvent) -> void:
 func show_game_over() -> void:
 	var scene := GAME_OVER_SCENE.instantiate()
 	add_child(scene)
+	
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(score_label, "scale", Vector2.ONE * 3, 1.0)
 
 func update_score() -> void:
 	score_label.text = str(Game.score)
+	
+	# Every 10 points
+	if Game.score % 10 == 0:
+		const DURATION : float = 0.3
+		
+		var tween = create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_SINE)
+		tween.tween_property(score_label, "scale", Vector2.ONE * 1.5, DURATION)
+		tween.tween_property(score_label, "scale", Vector2.ONE, DURATION)
 
 func show_player_dead(player : CharacterBody2D) -> void:
 	var scene := PLAYER_DEAD.instantiate()
